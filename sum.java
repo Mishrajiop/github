58f6d4d4d6ad6ecda68dce28e5ceccef6c2e6903
Bharat <mishrabharat167@gmail.com>
2023-01-02 15:59:22 +0530

    new git

--- sum.java
+++ sum.java
@@ -1,9 +1,9 @@
 class sum{
     public static void main(String args()){
-        int a;
-        int b ;
+        int a=1;
+        int b=2 ;
         int c=a+b;
-        
+        System.out.println(c);
 
     }
 }
